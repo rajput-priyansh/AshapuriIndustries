c7c93ffd23b98a24e6c19a0dc17d14a5916bcf05
Priyansh rajput <priyanshrajput38@gmail.com>
2021-04-03 13:38:07 +0530

    created order modules pdf

--- manage.py
+++ manage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AshapuriIndustries.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AshapuriIndustries.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
         try:
             import django
         except ImportError:
@@ -20,7 +20,3 @@ def main():
             )
         raise
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
